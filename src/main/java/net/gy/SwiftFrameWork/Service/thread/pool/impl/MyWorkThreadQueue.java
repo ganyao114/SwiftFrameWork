219389968224bb6211f81729c8faa3587283c740
ganyao114 <939543405@qq.com>
2016-08-09 13:53:02 +0800

    gy

--- src/main/java/net/gy/SwiftFrameWork/Service/thread/pool/impl/MyWorkThreadQueue.java
+++ src/main/java/net/gy/SwiftFrameWork/Service/thread/pool/impl/MyWorkThreadQueue.java
@@ -3,6 +3,9 @@ package net.gy.SwiftFrameWork.Service.thread.pool.impl;
 import net.gy.SwiftFrameWork.Service.thread.pool.control.ThreadPoolControl;
 import net.gy.SwiftFrameWork.Service.thread.pool.strategy.ExecutorThreadPool;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
+
 public class MyWorkThreadQueue {
 
     private static ThreadPoolControl mythreadpool;
@@ -13,10 +16,10 @@ public class MyWorkThreadQueue {
     }
 
 
-    public static void AddTask(Runnable r) {
+    public static Future AddTask(Runnable r) {
         if (mythreadpool == null)
             initPool();
-        mythreadpool.submit(r);
+        return mythreadpool.submit(r);
     }
 
 }
